eb669ba88b30cdf41f038bd37a95a8cc37a9583f
Jonathan Dubé <jonathandube@hotmail.fr>
2017-05-23 21:12:01 -0400

    Ajout de lecture et pause d'une chanson (path codé en dur pour le moment) avec mediaplayer

--- Musicalsystemclient/app/src/main/java/hellindustries/musicalsystemclient/PlayerActivity.java
+++ Musicalsystemclient/app/src/main/java/hellindustries/musicalsystemclient/PlayerActivity.java
@@ -1,11 +1,19 @@
 package hellindustries.musicalsystemclient;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -21,6 +29,8 @@ public class PlayerActivity extends AppCompatActivity {
     TextView songNameTxt;
     TextView artistTxt;
 
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +38,30 @@ public class PlayerActivity extends AppCompatActivity {
 
         getUIComponents();
         setOnClickListeners();
+
+        // Initiate MediaPlayer
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath() + "/Music/Liberee delivree.mp3");
+        try {
+            mediaPlayer.setDataSource(getBaseContext(), uri);
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // For seekbar updates during play
+        final Handler handler = new Handler();
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setMax(mediaPlayer.getDuration());
+        PlayerActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                int currentTime = mediaPlayer.getCurrentPosition();
+                seekBar.setProgress(currentTime);
+                handler.postDelayed(this, 1000);
+            }
+        });
 
     }
 
@@ -50,7 +84,7 @@ public class PlayerActivity extends AppCompatActivity {
         playPauseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                playPause();
             }
         });
         nextBtn.setOnClickListener(new View.OnClickListener() {
@@ -83,5 +117,20 @@ public class PlayerActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    /**
+     * Method that do play or do pause
+     */
+    private void playPause(){
+        Log.d("CLICK", "playPauseBtn clicked");
+
+        if(!mediaPlayer.isPlaying()){
+            mediaPlayer.start();
+            playPauseBtn.setImageResource(R.drawable.ic_pause_black_24dp);
+        } else {
+            mediaPlayer.pause();
+            playPauseBtn.setImageResource(R.drawable.ic_play_arrow_black_24dp);
+        }
     }
 }
