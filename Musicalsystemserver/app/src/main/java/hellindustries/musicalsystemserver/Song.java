678fae3720b7694919895aeb6f394dcae1e45a91
Jonathan Dubé <jonathandube@hotmail.fr>
2017-06-08 18:23:57 -0400

    [Server] Ajout du modèle Song

---
+++ Musicalsystemserver/app/src/main/java/hellindustries/musicalsystemserver/Song.java
@@ -0,0 +1,63 @@
+package hellindustries.musicalsystemserver;
+
+/**
+ * Created by Jonathan on 2017-06-08.
+ */
+
+public class Song {
+
+    private String id;
+    private String title;
+    private String artist;
+    private String album;
+    private String duration;
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
