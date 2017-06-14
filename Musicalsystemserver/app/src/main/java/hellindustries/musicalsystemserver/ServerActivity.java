8590e592b885ed99388820d6ddc46d9dd1557761
Unknown <mathieubelanger14@gmail.com>
2017-06-14 12:28:04 -0400

    [Server] Adding Listview of songs streamed on server interface

--- Musicalsystemserver/app/src/main/java/hellindustries/musicalsystemserver/ServerActivity.java
+++ Musicalsystemserver/app/src/main/java/hellindustries/musicalsystemserver/ServerActivity.java
@@ -3,14 +3,30 @@ package hellindustries.musicalsystemserver;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.AndroidTestCase;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Switch;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerActivity extends AppCompatActivity {
 
     private final static int REQUEST_EXTERNAL_STORAGE = 50;
+    private Switch serviceToggle;
+    private ListView songsListView;
+    private List<String> songArray;
+    private File[] musicFiles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +36,15 @@ public class ServerActivity extends AppCompatActivity {
 
         Intent serviceIntent = new Intent(this, MusicService.class);
         startService(serviceIntent);
+        songsListView = (ListView) findViewById(R.id.songsListView);
+        musicFiles = new File(Environment.getExternalStorageDirectory().getPath() + "/Music/").listFiles();
+        populateSongArray();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songArray);
+        songsListView.setAdapter(arrayAdapter);
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(this, MusicService.class));
+        super.onDestroy();
     }
 
     private void verifyStoragePermission() {
@@ -31,5 +56,38 @@ public class ServerActivity extends AppCompatActivity {
                     new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                     REQUEST_EXTERNAL_STORAGE);
         }
+    }
+
+    private void populateSongArray() {
+        songArray = new ArrayList<>();
+        for (int i = 0; i < musicFiles.length; i++) {
+
+            // Get song uri
+            Uri uri = Uri.parse(musicFiles[i].getPath());
+
+            // Create song object
+            Song song = this.createSongObject(i, uri);
+
+            // Add song to list
+            songArray.add(song.getTitle() + " - " + song.getArtist());
+
+        }
+    }
+
+    private Song createSongObject(int id, Uri uri){
+
+        // Set media metadate retriever
+        MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
+        mediaMetadataRetriever.setDataSource(getBaseContext(), uri);
+
+        // Create Song object
+        Song song = new Song();
+        song.setId(id + "");
+        song.setTitle(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE));
+        song.setArtist(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST));
+        song.setAlbum(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM));
+        song.setDuration(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+
+        return song;
     }
 }
